6edf6cfbf0be819f79ce2f924428d4abf7885b60
Mara <maragarciamartinez@gmail.com>
2025-01-28 14:51:51 +0100

    17

--- endpoints.md
+++ endpoints.md
@@ -1,5 +1,9 @@
 # Definició dels EndPoints del Servei Web:
 
+| Descripció | End-point | Method | Parametres | Resposta|
+|--------------|--------------|--------------|--------------|--------------|
+| Servei que consulta un User per Username | /users/getUser | GET | username | Code 200 Ok: {id=1,"username":"mara", "password":"garcia", "email":"maragm@gmail.com"}, Code 400 No trobat: {"error": "No trobat"} |
+
 - Descripció: Servei que consulta un User per Username
 - End-point: 192.168.144.131:10050/users/getUser
 - Host: 192.168.144.131:10050
---
+++ sv.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+
+class User:
+    def __init__(self, id, username, password, email=""):
+        self.id=id
+        self.username=username
+        self.password=password
+        self.email=email
+
+def __str__(self):
+    return "Id:" + str(self.id) + " Username:" + self.username
+
+listUsers= [
+    User(1,"usuari1", "12345", "user@gmail.com"),
+    User(2,"usuari2", "6789", "user2@gmail.com"),
+    User(3,"usuari3","0101","user3@gamail.com"),
+    User(4,"usuari4","2222")
+]
+
+class DAOUsers:
+    def __init__(self):
+        self.users=listUsers
+
+def getUserByUsername(self,username):
+    for u in self.users:
+        if u.username == username:
+            return u
+        return None
+
+daoUser = DAOUsers()
+
+'''u=daoUser.getUserByUsername("usuari1")
+if(u):
+    print(u)
+else:
+    print("No trobat")'''
+
+app = Flask(__name__)
+
+@app.route('/tapatapp/getuser', methods=['GET'])
+def getUser():
+    return "Im Mara García"
+
+def getUser():
+    n = str(request.args.get('name'))
+    email = str(request.args.get('email'))
+    return "Hello World Nombre: " + n + " Email: " + email
+
+@app.route('/prototip/getuser/<string:username>',methods=['GET'])
+def prototipGetUser(username):
+    return "Prototip 1, user:" + username
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port="10050")
